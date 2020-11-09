262d2f55c3336c2cbb826d36e9aafbeb82445743
Guihaierbai <15821107216@163.com>
2020-11-09 20:29:11 +0800

    11/5 UI框架（一）

---
+++ frame/demo/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+
+from frame.demo import hello
+from frame.demo.context import Context
+
+
+class TestMain:
+    context = Context()
+    context.load("./tmp.yaml")
+
+    @pytest.mark.parametrize("testcase", context.store.testcase.values(), ids=context.store.testcase.keys())
+    def test_main(self, testcase):
+        self.context.run_steps_by_testcase(testcase)
---
+++ frame/demo/testcase.py
@@ -0,0 +1,10 @@
+from frame.demo.context_base import ContextBase
+
+
+class TestCase(ContextBase):
+    def __init__(self, steps, context):
+        self.steps = steps
+        self._context = context
+
+    def run_steps(self):
+        self.get_context().run_steps(self.steps)
