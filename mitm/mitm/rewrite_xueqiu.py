27427d265742c4cdb0988096e82e9ec556e14a16
Guihaierbai <15821107216@163.com>
2020-12-01 21:04:22 +0800

    11/29雪球mock（一）作业

--- mitm/mitm/rewrite_xueqiu.py
+++ mitm/mitm/rewrite_xueqiu.py
@@ -6,7 +6,10 @@ def response(flow: http.HTTPFlow):
     if "quote.json" in flow.request.pretty_url and "x=" in flow.request.pretty_url:
         # 将响应数据转换成Python对象，保存到data中
         data = json.loads(flow.response.content)
-        # 修改第一支股票的名称
-        data['data']['items'][0]['quote']['name'] = "hogwarts1111111"
+        # 第一支股票保持原样
+        # 第二支股票名字加长一倍
+        data['data']['items'][1]['quote']['name'] *= 2
+        # 第三支股票名字为空
+        data['data']['items'][2]['quote']['name'] = ''
         # 将data的数值类型从字典转换成字符串并赋值给response原始数据格式
         flow.response.text = json.dumps(data)
